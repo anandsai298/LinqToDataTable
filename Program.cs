57d12cb9e362601d224a61257cdaec74c57edc72
anandsai298 <anandsai298@gmail.com>
2023-04-25 08:12:39 +0530

    [sai] Add : LinqToDataTable

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data;
+
+namespace Linq_To_DataTable;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("LINQ To DATATABLE");
+        DataTable dataTable = new DataTable();
+        dataTable.Columns.Add("ID");
+        dataTable.Columns.Add("ProductName");
+        dataTable.Rows.Add("1", "MI");
+        dataTable.Rows.Add("2", "REALME");
+        dataTable.Rows.Add("3", "VU");
+        Display(dataTable);
+    }
+    static void Display(DataTable dataTable)
+    {
+        var result=from product in dataTable.AsEnumerable() select product.Field<string>("ProductName");
+        Console.WriteLine(result);
+        foreach(var data in result)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
+
